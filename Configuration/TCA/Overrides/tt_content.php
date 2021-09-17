d97f20fe80a6552d059145e1a85dc8bd4a448f88
Konstantin Schneider <k.schneider@s-w-e.com>
2021-09-18 00:42:07 +0200

    [update]

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -20,4 +20,52 @@ call_user_func(function () {
             )
     );
 
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+        'tt_content',
+        [
+            'frame_class_extra' => [
+                'exclude' => 1,
+                'label' => 'LLL:EXT:lynx/Resources/Private/Language/locallang_be.xlf:frame.extra',
+                'description' => '',
+                'config' => [
+                    'type' => 'select',
+                    'renderType' => 'selectSingle',
+                    'items' => [
+                        ['Default', ''],
+                    ],
+                ],
+            ],
+            'breakpoint' => [
+                'exclude' => 1,
+                'label' => 'Breakpoint',
+                'description' => '',
+                'config' => [
+                    'type' => 'select',
+                    'renderType' => 'selectSingle',
+                    'items' => [
+                        ['XS', 'xs'],
+                        ['SM', 'sm'],
+                        ['MD', 'md'],
+                        ['LG', 'lg'],
+                        ['XL', 'xl'],
+                        ['XXL', 'xxl'],
+                    ],
+                ],
+            ]
+        ]
+    );
+
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'tt_content',
+        'frame_class_extra, breakpoint',
+        'appearance',
+        'after:space_after_class'
+    );
+
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
+        'tt_content',
+        'frames',
+        '--linebreak--, frame_class_extra, breakpoint'
+    );
+
 });
