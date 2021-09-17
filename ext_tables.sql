cbd90ab6675c89a7bee83472713c6a90a8388dc3
Konstantin Schneider <k.schneider@s-w-e.com>
2021-09-18 00:44:12 +0200

    [update]

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,8 @@
 #
 # Add SQL definition of database tables
 #
+CREATE TABLE tt_content
+(
+    frame_class_extra VARCHAR(30) DEFAULT '' NOT NULL,
+    breakpoint        VARCHAR(30) DEFAULT '' NOT NULL
+);
