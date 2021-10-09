87cdc783624590b140bf5cc515749071e1053afb
Konstantin Schneider <k.schneider@s-w-e.com>
2021-10-09 15:23:46 +0200

    [update]

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,6 +5,8 @@ CREATE TABLE tt_content
 (
     container        VARCHAR(30) DEFAULT '' NOT NULL,
     breakpoint       VARCHAR(30) DEFAULT '' NOT NULL,
+    padding_top      VARCHAR(30) DEFAULT '' NOT NULL,
+    padding_bottom   VARCHAR(30) DEFAULT '' NOT NULL,
     background_color VARCHAR(30) DEFAULT '' NOT NULL,
     foreground_color VARCHAR(30) DEFAULT '' NOT NULL
 );
