53564d78d1b921fa416c2f2c50d278773d3e574c
Konstantin Schneider <k.schneider@s-w-e.com>
2021-10-16 19:08:04 +0200

    [update]

---
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Basic value for new Site Configurations
+ */
+$GLOBALS['SiteConfiguration']['site_language']['columns']['title']['config']['default'] = 'Deutsch';
+$GLOBALS['SiteConfiguration']['site_language']['columns']['typo3Language']['config']['default'] = 'de';
+$GLOBALS['SiteConfiguration']['site_language']['columns']['locale']['config']['default'] = 'de_DE.UTF-8';
+$GLOBALS['SiteConfiguration']['site_language']['columns']['iso-639-1']['config']['default'] = 'de';
+$GLOBALS['SiteConfiguration']['site_language']['columns']['websiteTitle']['config']['default'] = 'Lynx';
+$GLOBALS['SiteConfiguration']['site_language']['columns']['navigationTitle']['config']['default'] = 'Deutsch';
+$GLOBALS['SiteConfiguration']['site_language']['columns']['hreflang']['config']['default'] = 'de-DE';
+$GLOBALS['SiteConfiguration']['site_language']['columns']['direction']['config']['default'] = 'ltr';
+$GLOBALS['SiteConfiguration']['site_language']['columns']['flag']['config']['default'] = 'de';
