026acfb17e758a0ed6e4102962d3f5fb49cda935
Konstantin Schneider <k.schneider@s-w-e.com>
2021-10-24 14:20:12 +0200

    [update]

--- Classes/PreviewRenderer.php
+++ Classes/PreviewRenderer.php
@@ -2,31 +2,14 @@
 
 namespace Vendor\Skeleton;
 
-use TYPO3\CMS\Backend\Preview\PreviewRendererInterface;
+use TYPO3\CMS\Backend\Preview\StandardContentPreviewRenderer;
 use TYPO3\CMS\Backend\View\BackendLayout\Grid\GridColumnItem;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
-class PreviewRenderer implements PreviewRendererInterface {
-
-    public function renderPageModulePreviewHeader(GridColumnItem $item): string
-    {
-        return '';
-    }
-
-    public function renderPageModulePreviewContent(GridColumnItem $item): string
-    {
-        return '';
-    }
-
+class PreviewRenderer extends StandardContentPreviewRenderer
+{
     public function renderPageModulePreviewFooter(GridColumnItem $item): string
     {
-        DebuggerUtility::var_dump($item);
-
-        return 'Test';
-    }
-
-    public function wrapPageModulePreview(string $previewHeader, string $previewContent, GridColumnItem $item): string
-    {
-        return 'Test';
+        echo $item;
+        return parent::renderPageModulePreviewFooter($item);
     }
 }
