df67888ec0973bcd420022adf4265e5f2f0de232
Konstantin Schneider <schneider.konstantin@outlook.com>
2023-11-19 21:15:45 +0100

    add Icons.php

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+   'skeleton-text' => [
+      'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+      'source' => 'EXT:skeleton/Resources/Public/Icons/Extension.svg',
+   ],
+];
