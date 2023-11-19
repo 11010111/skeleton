40da11371a59757d4edf990ed59424c2c0dd1a9f
Konstantin Schneider <schneider.konstantin@outlook.com>
2023-11-19 21:17:36 +0100

    improvements

--- Resources/Public/JavaScript/Src/main.js
+++ Resources/Public/JavaScript/Src/main.js
@@ -1,26 +1,26 @@
 // Imports
 import "../../Css/tailwind.css"
-import {
-    containerColors,
-    mobileMenu,
-    navigationFixed,
-    convertImgSvg,
-    equalHeight,
-    parallax,
-    scrollToTop
-} from "./modules.js"
+// import {
+//     containerColors,
+//     mobileMenu,
+//     navigationFixed,
+//     convertImgSvg,
+//     equalHeight,
+//     parallax,
+//     scrollToTop
+// } from "./modules.js"
 
-/**
- * Module inits
- */
-containerColors.init()
-mobileMenu.init()
-navigationFixed.init()
-convertImgSvg.init()
-equalHeight.init()
-//equalHeight.init('.equal-height', '.example-group')
-//equalHeight.init('.example-class', false)
-parallax.init()
-scrollToTop.init()
+// /**
+//  * Module inits
+//  */
+// containerColors.init()
+// mobileMenu.init()
+// navigationFixed.init()
+// convertImgSvg.init()
+// equalHeight.init()
+// //equalHeight.init('.equal-height', '.example-group')
+// //equalHeight.init('.example-class', false)
+// parallax.init()
+// scrollToTop.init()
 
 console.log('WE LOVE TYPO3')
