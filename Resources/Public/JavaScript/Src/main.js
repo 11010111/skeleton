0a6fa0d958f8c38f1b3d5d86502224d62c029c33
Konstantin Schneider <schneider.konstantin@outlook.com>
2023-11-20 18:18:17 +0100

    mobile menu + scroll to top

--- Resources/Public/JavaScript/Src/main.js
+++ Resources/Public/JavaScript/Src/main.js
@@ -1,26 +1,9 @@
 // Imports
 import "../../Css/tailwind.css"
-// import {
-//     containerColors,
-//     mobileMenu,
-//     navigationFixed,
-//     convertImgSvg,
-//     equalHeight,
-//     parallax,
-//     scrollToTop
-// } from "./modules.js"
+import moreMenu from "./moreMenu"
+import scrollTop from "./scrollTop"
 
-// /**
-//  * Module inits
-//  */
-// containerColors.init()
-// mobileMenu.init()
-// navigationFixed.init()
-// convertImgSvg.init()
-// equalHeight.init()
-// //equalHeight.init('.equal-height', '.example-group')
-// //equalHeight.init('.example-class', false)
-// parallax.init()
-// scrollToTop.init()
+moreMenu()
+scrollTop()
 
 console.log('WE LOVE TYPO3')
--- Resources/Public/Out/main.bundle.js
+++ Resources/Public/Out/main.bundle.js
@@ -1,1 +1,26 @@
+const l = () => {
+  const e = document.querySelector(".navigation-list"), o = document.querySelector(".level1-more-btn"), t = document.querySelector(".level1-more-wrapper");
+  if (!o || !t || !e)
+    return;
+  o.addEventListener("click", () => {
+    t.style.bottom = e.clientHeight + "px", t.classList.toggle("level1-more-open");
+  });
+  const n = () => {
+    t.style.bottom = e.clientHeight + "px";
+  };
+  n(), window.addEventListener("resize", n);
+}, r = () => {
+  const e = document.querySelector(".scroll-top");
+  if (!e)
+    return;
+  e.addEventListener("click", () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  });
+  const o = () => {
+    window.scrollY > window.innerHeight ? e.classList.add("scroll-top-show") : e.classList.remove("scroll-top-show");
+  };
+  window.addEventListener("scroll", o);
+};
+l();
+r();
 console.log("WE LOVE TYPO3");
