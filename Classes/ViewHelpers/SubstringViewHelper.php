d7d5f75c03e987ca04196cda7a9cc12c6762e5a7
Konstantin Schneider <schneider.konstantin@outlook.com>
2023-11-21 21:48:14 +0100

    add substring viewhelper

---
+++ Classes/ViewHelpers/SubstringViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Konstantinschneider\Skeleton\ViewHelpers;
+
+use Closure;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class SubstringViewHelper
+ *
+ * @package Konstantinschneider\Skeleton\ViewHelpers
+ */
+class SubstringViewHelper extends AbstractViewHelper
+{
+  /**
+   * @inheritDoc
+   */
+  public function initializeArguments()
+  {
+    $this->registerArgument('string', 'string', 'The enter string', true);
+    $this->registerArgument('offset', 'int', 'The offset', true);
+    $this->registerArgument('length', 'int', 'The length');
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function renderStatic(
+    array                     $arguments,
+    Closure                   $renderChildrenClosure,
+    RenderingContextInterface $renderingContext
+  )
+  {
+    if (empty($arguments['length'])) {
+      return substr($arguments['string'], $arguments['offset']);
+    } else {
+      return substr($arguments['string'], $arguments['offset'], $arguments['length']);
+    }
+  }
+}
