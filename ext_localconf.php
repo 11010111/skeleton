3eaab1187a6b44797e8df6f866d4fc85c4ec8d55
Konstantin Schneider <schneider.konstantin@outlook.com>
2023-12-03 00:11:11 +0100

    add backend css

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,3 +11,8 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['skeleton'] = 'EXT:skeleton/Config
  * Add global skeleton namespace
  */
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['skeleton'] = ['KSNC\\Skeleton\\ViewHelpers'];
+
+/***************
+ * Add backend CSS
+ */
+$GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['skeleton'] = 'EXT:skeleton/Resources/Public/Css/backend.css';
